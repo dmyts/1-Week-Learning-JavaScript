0d3f43463079144e8b983fcc37d8bb32ce026ba7
dmyts <danaraga101@gmail.com>
2024-11-09 18:10:23 +0700

    Learn how to declare variables in JS

---
+++ variable.js
@@ -0,0 +1,6 @@
+// Learn Variable in JS
+var x = 10; // Variabel global atau fungsi (hindari penggunaan 'var' jika tidak perlu)
+let y = 20; // Variabel yang dapat di ubah
+const z = 30; // Variabel konstan yang nilainya tidak bisa diubah
+
+
