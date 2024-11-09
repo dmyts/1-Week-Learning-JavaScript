cf6884d725cf1ccaf180c6438c1e108c4301d7cc
dmyts <danaraga101@gmail.com>
2024-11-09 18:21:57 +0700

    Learn data types for variables in JS

--- variable.js
+++ variable.js
@@ -3,4 +3,10 @@ var x = 10; // Variabel global atau fungsi (hindari penggunaan 'var' jika tidak 
 let y = 20; // Variabel yang dapat di ubah
 const z = 30; // Variabel konstan yang nilainya tidak bisa diubah
 
-
+// Learn data types for variables in JS
+let name = "Dana Raga"; // String
+let age = 16; // Integer
+let isStudent = true; // Boolean
+let score = null; // Null
+let grade; // Undefined
+let user = {name: "Dana", age: 16}; // Object
