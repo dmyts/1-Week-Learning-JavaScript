21eb09a06f2c504d16addd38ee0ce7c515bc9731
dmyts <danaraga101@gmail.com>
2024-11-13 15:01:27 +0700

    Learning about Object and Array

---
+++ object-and-array.js
@@ -0,0 +1,17 @@
+// Learning about Object and Array in JavaScript
+
+// Const
+const person = {
+  name: "Dana Raga",
+  age: 16,
+  isStudent: true,
+  greet: function() {
+    return `Hello, my name is ${this.name}`;
+  }
+}
+
+console.log(person); // Output: Dana Raga
+console.log(person.greet()); // Output: Hello, my name is Dana Raga
+
+person.school = "Vocational High School";
+person.age = 17;
