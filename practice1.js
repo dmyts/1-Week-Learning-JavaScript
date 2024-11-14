5413d15a8b16c101156e1f3b8506fa27132329c0
dmyts <danaraga101@gmail.com>
2024-11-14 08:30:46 +0700

    Creating Student Data

---
+++ practice1.js
@@ -0,0 +1,8 @@
+// Creating Student Data
+// First, create an array containing several student objects:
+
+const students = [
+  { name: "Dana Raga", age: 16, class: "XI PPLG 2" },
+  { name: "Fulan", age: 16, class: "XI PPlG 1" },
+  { name: "Fulana", age: 16, class: "XI PPLG 3" }
+];
